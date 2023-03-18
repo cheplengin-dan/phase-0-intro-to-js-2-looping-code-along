ee72e7a8b616d33b6e186f8ff58b9028a65c9e5b
Duncan <cheplenginduncan@gmail.com>
2023-03-18 11:56:21 +0300

    completedLoopingCodeALong

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const myArrCards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+    for ( let i = 0; i < myArrCards.length; i++) {
+        return [`Thank you, ${myArrCards[0]}, for the wonderful surprise gift!`,
+        `Thank you, ${myArrCards[1]}, for the wonderful surprise gift!`,
+        `Thank you, ${myArrCards[2]}, for the wonderful surprise gift!`];
+    }
+
+}
+
+function countDown() {
+
+    let countDown = 0;
+
+    while ( countDown < 11) {
+        console.log(countDown++);
+    }
+
+
+}
